7efe765906aa2e909ded5687ed254d6ff30022f3
Joon Min <joongi.min@gmail.com>
2015-02-16 11:10:38 -0500

    Revert "Removed alias_method_chain render_with_remotipart and required it to be called manually"
    This reverts commit 6c1282e9b8c0af0136ae0bf9e82f6531ab8ca8ac.

--- lib/remotipart/render_overrides.rb
+++ lib/remotipart/render_overrides.rb
@@ -4,15 +4,14 @@ module Remotipart
   module RenderOverrides
     include ERB::Util
 
-    # 141204, Joon Min - Call render_with_remotipart manually only when required to save function call overhead
-    # def self.included(base)
-    #   base.class_eval do
-    #     alias_method_chain :render, :remotipart
-    #   end
-    # end
+    def self.included(base)
+      base.class_eval do
+        alias_method_chain :render, :remotipart
+      end
+    end
 
-    def render_with_remotipart(*args)
-      render(*args)
+    def render_with_remotipart *args
+      render_without_remotipart *args
       if remotipart_submitted?
         textarea_body = response.content_type == 'text/html' ? html_escape(response.body) : response.body
         response.body = %{<textarea data-type=\"#{response.content_type}\" data-status=\"#{response.response_code}\" data-statusText=\"#{response.message}\">#{textarea_body}</textarea>}
